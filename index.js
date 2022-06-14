b97d1ca42be11bbee8c6858b1dd0e7106c86d4c1
DanniD33 <danni.denmark7@gmail.com>
2022-06-14 16:03:58 -0400

    Serving homepage of SysOrThat

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const { resolveSoa } = require("dns");
 const express = require("express");
 const { appendFile } = require("fs");
+const { dirname } = require("path");
 const server = express();
 const PORT = process.env.PORT || 3000
 
+//to serve static file, we still need to use our methods to determine what it is we want to do with said file once we've served it 
+// server.use(express.static(__dirname + '/Scenarios/index.html'))
+
 server.get("/", (req,res) => {
-  res.send("What up tho")
+  res.sendFile(__dirname + '/Scenarios/index.html');
 })
 
 server.listen(PORT, () =>{
