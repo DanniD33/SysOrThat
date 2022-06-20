701bb7af95b5cb5ac91f4bfb5ae351861c147f76
DanniD33 <danni.denmark7@gmail.com>
2022-06-20 16:13:47 -0400

    included system desgin componenets in api and created client side architecture folder to serve later

---
+++ src/routes/clientside.js
@@ -0,0 +1,10 @@
+//Client Server Architecturee
+
+  //we will need to decide between 2 tier and 3 tier
+
+const clientSide = () => {
+  console.log('Select 2 Tier or 3 Tier')
+}
+
+  // const choice = module.exports() // returns whatever our output is to the server
+  
