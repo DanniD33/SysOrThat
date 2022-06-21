e038452b8f797e7f1bb23ec876bc5f42342b1ab7
DanniD33 <danni.denmark7@gmail.com>
2022-06-21 17:03:17 -0400

    added sudo code for clientSide File

--- src/routes/clientside.js
+++ src/routes/clientside.js
@@ -3,7 +3,19 @@
   //we will need to decide between 2 tier and 3 tier
 
 const clientSide = () => {
-  console.log('Select 2 Tier or 3 Tier')
+  console.log("You have entered in client server architecture. Here we manage requests and responses between the client and the server. Sometimes, we're asked to manipulate some of the data on the server side before we send responses back but we love it around these parts. Let's take a closer look.")
+  //create a function that takes in the AMOUNT of data we're dealing with
+  //this will determine whether we trigger dialogue for two tier or three
+  function howMuchData(total){
+    if (total > )
+    
+  }
+  // create a function that takes in an input regarding the type of data we're dealing with that begins dialogue about  
+  //we will then create a function that triggers a function that returns a true or false value 
+    //the returned boolean will determine whether the logic will sit on the server side or the client side
+      //we will then need to trigger dialouge about:
+      // thick client meaning procesing logic is sitting on client side (video editing software)
+      // thin client meaning processing logic is sitting on the server side (streaming services like netflix) 
 }
 
   // const choice = module.exports() // returns whatever our output is to the server
