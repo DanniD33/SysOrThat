0f7c0774d6f610f311dae4277ee5b35e4df1e10a
DanniD33 <danni.denmark7@gmail.com>
2022-06-23 11:03:01 -0400

    need to install fetch package to fetch choices api successfully

--- src/routes/clientside.js
+++ src/routes/clientside.js
@@ -6,10 +6,10 @@ const clientSide = () => {
   console.log("You have entered in client server architecture. Here we manage requests and responses between the client and the server. Sometimes, we're asked to manipulate some of the data on the server side before we send responses back but we love it around these parts. Let's take a closer look.")
   //create a function that takes in the AMOUNT of data we're dealing with
   //this will determine whether we trigger dialogue for two tier or three
-  function howMuchData(total){
-    if (total > )
+  // function howMuchData(total){
+  //   if (total > )
     
-  }
+  // }
   // create a function that takes in an input regarding the type of data we're dealing with that begins dialogue about  
   //we will then create a function that triggers a function that returns a true or false value 
     //the returned boolean will determine whether the logic will sit on the server side or the client side
